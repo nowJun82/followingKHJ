64cc0acde50d03706f96e512ae570ff44febd949
kimhyeji <gpwl5589@gmail.com>
2023-08-14 13:31:14 +0900

    회원가입시, 입력데이터 유효성 체크

--- src/main/java/com/khj/exam/demo/controller/UsrMemberController.java
+++ src/main/java/com/khj/exam/demo/controller/UsrMemberController.java
@@ -19,6 +19,34 @@ public class UsrMemberController {
 	@ResponseBody
 	public Object doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNo,
 			String email) {
+		if ( loginId == null || loginId.trim().length() == 0 ) {
+			return "loginId(을)를 입력해주세요.";
+		}
+		
+		if ( loginId == null || loginId.trim().length() == 0 ) {
+			return "loginId(을)를 입력해주세요.";
+		}
+		
+		if ( loginPw == null || loginPw.trim().length() == 0 ) {
+			return "loginPw(을)를 입력해주세요.";
+		}
+		
+		if ( name == null || name.trim().length() == 0 ) {
+			return "name(을)를 입력해주세요.";
+		}
+		
+		if ( nickname == null || nickname.trim().length() == 0 ) {
+			return "nickname(을)를 입력해주세요.";
+		}
+		
+		if ( cellphoneNo == null || cellphoneNo.trim().length() == 0 ) {
+			return "cellphoneNo(을)를 입력해주세요.";
+		}
+		
+		if ( email == null || email.trim().length() == 0 ) {
+			return "email(을)를 입력해주세요.";
+		}
+		
 		int id = memberService.join(loginId, loginPw, name, nickname, cellphoneNo, email);
 		
 		if ( id == -1 ) {
