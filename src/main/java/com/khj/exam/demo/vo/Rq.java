11aa33c9b0b9f5f337c41f55a258132cbb9df213
kimhyeji <gpwl5589@gmail.com>
2023-08-18 13:20:23 +0900

    BeforeActionInterceptor 구현

--- src/main/java/com/khj/exam/demo/vo/Rq.java
+++ src/main/java/com/khj/exam/demo/vo/Rq.java
@@ -12,6 +12,7 @@ public class Rq {
 	
 	public Rq(HttpServletRequest req) {
 		HttpSession httpSession = req.getSession();
+		
 		boolean isLogined = false;
 		int loginedMemberId = 0;
 		
@@ -23,9 +24,5 @@ public class Rq {
 		this.isLogined = isLogined;
 		this.loginedMemberId = loginedMemberId;
 		
-	}
-	
-	public boolean isLogined() {
-		return false;
 	}
 }
